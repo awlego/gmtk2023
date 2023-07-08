666b2144a7cda1215b32071e51b5c574b0126afa
Alex Wilson <awlego@gmail.com>
2023-07-07 23:48:42 -0700

    wip

---
+++ devsim/scenes/evil_corp/Project.gd
@@ -0,0 +1,20 @@
+extends Node
+
+class_name Project
+
+var text = ""
+var complete = false
+var money_cost = 0
+var research_cost = 0
+var project_index = 0
+var project_unlocks = 0
+
+
+func _init(_text="", _complete = false, _money_cost=0, _research_cost=0, _project_index=0, _project_unlocks=0):
+	text = _text
+	complete = _complete
+	money_cost = _money_cost
+	research_cost = _research_cost
+	project_index = _project_index
+	project_unlocks = _project_unlocks
+	
