70cfd378aa7628e2f08cda4c7fc8a1921b8def7e
Tim Meyer <dgnt22@gmail.com>
2023-07-07 21:01:47 -0700

    font added, refactor calls to main, debug warp buttons added.

--- devsim/scenes/MainScene.gd
+++ devsim/scenes/MainScene.gd
@@ -1,24 +1,24 @@
 extends MarginContainer
-var stage1 = preload("res://scenes/Stage1.tscn")
+var stage1_preload = preload("res://scenes/Stage1.tscn")
+var stage1 = stage1_preload.instance()
+var stage0
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# Declare member variables here.
 var clicks = 0
 var stage = 0
+var hours_enjoyed = 0
+var hours_message = "Hours of gameplay enjoyed: "
+var money = 1000
 var title_node
+var money_node
+var enjoy_node
 
 func click():
 	clicks += 1
 	update_title()
 	if (stage == 0 && clicks >= 10):
-		var v0 = get_node("v0")
-		var child_stage = get_node("v0/Stage0")
-		v0.remove_child(child_stage)
-		if is_instance_valid(child_stage):
-			child_stage.queue_free()
-		v0.add_child(stage1.instance())
-		register_stage()
+		stage = 1
+		set_stage(stage1)
 
 func _on_update_clicks(diff):
 	clicks += diff
@@ -26,17 +26,39 @@ func _on_update_clicks(diff):
 	
 func update_title():
 	title_node.text = "Clicks: " + str(clicks)
+
+func update_money(diff):
+	money += diff
+	money_node.text = "$" + str(money)
+
+func update_enjoy(hours):
+	hours_enjoyed += hours
+	enjoy_node.text = hours_message + str(hours)
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_node("v0/Stage0/Button").connect("pressed", self, "click")
+	setup_warp()
 	title_node = get_node("v0/TopBar/Title")
+	money_node = get_node("v0/TopBar/Money")
+	enjoy_node = get_node("v0/TopBar/Hours")
+	stage1.main = self
+	stage0 = get_node("v0/Stage0")
 	pass # Replace with function body.
 
-func register_stage():
+func setup_warp():
+	get_node("v0/Stage0/WarpS1").connect("pressed", self, "warpS1")
+
+func set_stage(stage):
 	var v0 = get_node("v0")
-	var stage = v0.get_child(1)
-	stage.connect("update_clicks", self, "_on_update_clicks")
+	if (v0.get_child_count() > 1):
+		v0.remove_child(v0.get_child(1))
+	v0.add_child(stage)
+
+func warpS1():
+	clicks = 10
+	update_title()
+	set_stage(stage1)
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- devsim/scenes/Stage1.gd
+++ devsim/scenes/Stage1.gd
@@ -1,11 +1,6 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-signal update_clicks(diff)
+var main
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,7 +8,7 @@ func _ready():
 	pass # Replace with function body.
 
 func _on_a1_click():
-	emit_signal("update_clicks", 4)
+	main._on_update_clicks(2)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
